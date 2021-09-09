96a7cfb511e1b1c1e09c3f56105723f9d123d8f1
Marco Gazerro <gazerro@open2b.com>
2021-09-09 10:11:23 +0200

    cmd: go mod vendor

---
+++ cmd/vendor/github.com/open2b/scriggo/README.md
@@ -0,0 +1,116 @@
+<img src="https://scriggo-site.pages.dev/images/scriggo.svg" alt="Scriggo" title="Scriggo" width="575px" style="max-width: 100%">
+
+The fast, modern and secure Go template engine and embeddable Go runtime.
+
+## Features
+
+* Fast, the fastest embeddable pure Go language runtime.
+* Modern and powerful Template Engine with Go as scripting language.
+* Native support for Markdown in templates.
+* Secure by default. No access to packages unless explicitly enabled.
+* Easy to embed and to interop with any Go application.
+
+## Get Started with Programs
+
+Execute a Go program embedded in your application:
+
+```go
+package main
+
+import "github.com/open2b/scriggo"
+
+func main() {
+
+    // src is the source code of the program to run.
+    src := []byte(`
+        package main
+
+        func main() {
+            println("Hello, World!")
+        }
+    `)
+
+    // Create a file system with the file of the program to run.
+    fsys := scriggo.Files{"main.go": src}
+
+    // Build the program.
+    program, err := scriggo.Build(fsys, nil)
+    if err != nil {
+        panic(err)
+    }
+ 
+    // Run the program.
+    err = program.Run(nil)
+    if err != nil {
+        panic(err)
+    }
+
+}
+```
+
+## Get Started with Templates
+
+Scriggo is also a modern and powerful template engine for Go, supporting inheritance,
+macros, partials, imports and contextual autoescaping but most of all it uses the
+Go language as the template scripting language.
+
+```
+{% extends "layout.html" %}
+{% import "banners.html" %}
+{% macro Body %}
+    <ul>
+      {% for _, product := range products %}
+      <li><a href="{{ product.URL }}">{{ product.Name }}</a></li>
+      {% end %}
+    </ul>
+    {{ render "pagination.html" }}
+    {{ Banner() }}
+{% end %}
+```
+
+Scriggo template files can be written in plain text, HTML, Markdown, CSS, JavaScript and JSON.
+
+### Execute a Scriggo template in your application
+
+```go
+// Build and run a Scriggo template.
+package main
+
+import (
+    "os"
+    "github.com/open2b/scriggo"
+)
+
+func main() {
+
+    // Content of the template file to run.
+    content := []byte(`
+    <!DOCTYPE html>
+    <html>
+    <head>Hello</head> 
+    <body>
+        {% who := "World" %}
+        Hello, {{ who }}!
+    </body>
+    </html>
+    `)
+
+    // Create a file system with the file of the template to run.
+    fsys := scriggo.Files{"index.html": content}
+
+    // Build the template.
+    template, err := scriggo.BuildTemplate(fsys, "index.html", nil)
+    if err != nil {
+        panic(err)
+    }
+ 
+    // Run the template and print it to the standard output.
+    err = template.Run(os.Stdout, nil, nil)
+    if err != nil {
+        panic(err)
+    }
+
+}
+```
+
+For a complete get started guide see the [Scriggo site](https://www.scriggo.com/).
---
+++ cmd/vendor/github.com/open2b/scriggo/errors.go
@@ -0,0 +1,111 @@
+// Copyright 2019 The Scriggo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scriggo
+
+import (
+	"strconv"
+
+	"github.com/open2b/scriggo/internal/compiler"
+	"github.com/open2b/scriggo/internal/runtime"
+)
+
+// Position is a position in a file.
+type Position struct {
+	Line   int // line starting from 1
+	Column int // column in characters starting from 1
+	Start  int // index of the first byte
+	End    int // index of the last byte
+}
+
+// String returns line and column separated by a colon, for example "37:18".
+func (p Position) String() string {
+	return strconv.Itoa(p.Line) + ":" + strconv.Itoa(p.Column)
+}
+
+// BuildError represents an error occurred building a program or template.
+type BuildError struct {
+	err compiler.Error
+}
+
+// Error returns a string representation of the error.
+func (err *BuildError) Error() string {
+	return err.err.Error()
+}
+
+// Path returns the path of the file where the error occurred.
+func (err *BuildError) Path() string {
+	return err.err.Path()
+}
+
+// Position returns the position in the file where the error occurred.
+func (err *BuildError) Position() Position {
+	pos := err.err.Position()
+	return Position{Line: pos.Line, Column: pos.Column, Start: pos.Start, End: pos.End}
+}
+
+// Message returns the error message.
+func (err *BuildError) Message() string {
+	return err.err.Message()
+}
+
+// ExitError represents an exit from an execution with a non-zero code. It is
+// returned by a Run method when the Exit method of native.Env is called with
+// a non-zero value.
+type ExitError int
+
+// Error returns a string representation of the error.
+func (err ExitError) Error() string {
+	return "exit code " + strconv.Itoa(int(err))
+}
+
+// Code returns the exit code.
+func (err ExitError) Code() int {
+	return int(err)
+}
+
+// PanicError represents the error that occurs when an executed program or
+// template calls the panic built-in or the Panic method of native.Env is
+// called and the panic is not recovered.
+type PanicError struct {
+	p *runtime.PanicError
+}
+
+// Error returns all currently active panics as a string.
+//
+// To print only the message, use the String method instead.
+func (p *PanicError) Error() string {
+	return p.p.Error()
+}
+
+// Message returns the panic message.
+func (p *PanicError) Message() interface{} {
+	return p.p.Message()
+}
+
+// Next returns the next panic in the chain.
+func (p *PanicError) Next() *PanicError {
+	return &PanicError{p.p.Next()}
+}
+
+// Recovered reports whether it has been recovered.
+func (p *PanicError) Recovered() bool {
+	return p.p.Recovered()
+}
+
+// String returns the panic message as a string.
+func (p *PanicError) String() string {
+	return p.p.String()
+}
+
+// Path returns the path of the file that panicked.
+func (p *PanicError) Path() string {
+	return p.p.Path()
+}
+
+// Position returns the position in file where the panic occurred.
+func (p *PanicError) Position() Position {
+	pos := p.p.Position()
+	return Position{Line: pos.Line, Column: pos.Column, Start: pos.Start, End: pos.End}
+}
